cabeb4d9f3366a5166a703f11d4f6f627d526e50
David Felipe <felipe30.10@outlook.com>
2023-01-06 17:59:48 -0300

    add weather

--- script.js
+++ script.js
@@ -39,5 +39,71 @@ clock()
 setInterval( clock, 500)
 
 
+const Apikey = 'fb9709843d4b5f15c6ffa23d37d77f85'
+
+async function api(url) {
+    const response = await fetch(url).then(response => response.json())
+    return response
+}
+
+async function getcity(city) {
+    const data = await api(`https://api.openweathermap.org/data/2.5/weather?q=${city}&limit=1&appid=${Apikey}`)
+    const result = data  ? {
+        description: `${data.name}, ${data.sys.country}`,
+        lat: data.coord.lat,
+        lon: data.coord.lon,
+    } : null;
+
+    console.log(result)
+    return result
+}
 
 
+async function getWeather(lat, lon){
+    const lang = "pt_br"
+    const units = "metric"
+    const data = await api(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${Apikey}&lang=${lang}&units=${units}`)
+    const result = data  ? {
+        description:  data.weather[0].description,
+        icon: data.weather[0].icon,
+        temperature: data.main.temp,
+    } : null;
+
+    return result
+
+}
+
+async function weather() {
+    
+    const valueCity = document.querySelector('input.city').value
+
+    if(!valueCity) {
+        return
+    }
+
+    const elementWeather = document.querySelector('.weather')
+    const weatherIcon = document.getElementById('weatherIcon')
+    const elementCity = document.getElementById('city')
+    const description = document.getElementById('description')
+    const temp = document.getElementById('temp')
+
+    elementWeather.classList.add('active')
+
+    const city = await getcity(valueCity)
+    const weather = await getWeather(city.lat, city.lon)
+
+    weatherIcon.attributes.src.value = `http://openweathermap.org/img/w/${weather.icon}.png`
+
+    elementCity.innerText = city.description
+    description.innerText = weather.description
+    temp.innerText = `${weather.temperature}°`
+
+}
+
+const form = document.getElementById('localization')
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+
+    weather()
+})
